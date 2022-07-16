5f1b2535e07ada80692296f8b1a3d5ba8330148b
bird <53257509+bird0401@users.noreply.github.com>
2022-07-16 19:01:46 +0900

    Update conda_python_3_9.sh

--- conda_python_3_9.sh
+++ conda_python_3_9.sh
@@ -1,4 +1,4 @@
 # create virtual env
-conda create -n python39 pip python=3.9
+conda create -n house_price_python39 pip python=3.9
 # activate the env
-conda activate python39
+conda activate house_price_python39
